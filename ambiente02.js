471de38c393c7ea47147d38b557aad7f4ec659b2
SAVIO GALVAO LACERDA FILHO <98129784+oivasadrecal@users.noreply.github.com>
2024-09-22 18:45:54 -0300

    Add files via upload

---
+++ ambiente02.js
@@ -0,0 +1,11 @@
+/*
+var c= 1
+while (c <= 5){
+    console.log(c)
+    c++
+}*/
+console.log('vai começar')
+for(var c =1;c <=10;c++){
+    console.log(c)
+}
+console.log('fim')
---
+++ aula 16/vetor.js
@@ -0,0 +1,8 @@
+var num = [5,2,9,3,4,8,6,7]
+num.push(1)
+num.sort()
+console.log(num)
+console.log(`o vetor tem ${num.length} posições`)
+console.log(`nosso vetor é ${num[0]}`)
+let pos = num.indexOf(8)
+console.log(`O valor 8 está na posição ${pos}`)
